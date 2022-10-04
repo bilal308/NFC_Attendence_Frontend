baaf81f5ecdbf23560a95e2e5c8e9163d13ba129
MUK-Dev <manian1221@gmail.com>
2022-10-04 22:33:04 +0500

    Register page Routing & animations working

---
+++ src/Components/auth/StudentForm.jsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import {
+  Button,
+  Divider,
+  IconButton,
+  LinearProgress,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { ArrowBackIos } from '@mui/icons-material';
+
+const StudentForm = ({ animation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const [_, setSearchParams] = useSearchParams();
+
+  const opacityAnimate = {
+    initial: {
+      // opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+    exit: {
+      // opacity: 0,
+    },
+  };
+
+  const opacityAnimateDivider = {
+    initial: {
+      // opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+    exit: {
+      background: 'rgb(200,171,169)',
+    },
+  };
+
+  return (
+    <Stack
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      width='100%'
+      component={motion.div}
+      key='StudentForm'
+      variants={animation}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      sx={{ position: 'relative' }}
+    >
+      <Typography variant='h5' gutterBottom>
+        Register as a Student
+      </Typography>
+      <AnimatePresence mode='wait'>
+        {isLoading ? (
+          <motion.div
+            key='Progress'
+            variants={opacityAnimate}
+            initial='initial'
+            animate='animate'
+            exit='exit'
+            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+          >
+            <LinearProgress
+              sx={{
+                width: '25%',
+                marginBottom: '1em',
+              }}
+            />
+          </motion.div>
+        ) : (
+          <Divider
+            key='Divider'
+            variants={opacityAnimateDivider}
+            initial='initial'
+            animate='animate'
+            exit='exit'
+            component={motion.div}
+            sx={{
+              background: '#70231d',
+              height: '4px',
+              width: '25%',
+              borderRadius: '50px',
+              display: 'flex',
+              justifyContent: 'center',
+              marginBottom: '1em',
+            }}
+          />
+        )}
+      </AnimatePresence>
+      <Stack
+        direction='column'
+        alignItems='center'
+        sx={{ paddingInline: '1em', width: '100%', gap: '1em' }}
+      >
+        <TextField variant='outlined' fullWidth label='Email' type='email' />
+        <TextField
+          variant='outlined'
+          fullWidth
+          label='Password'
+          type='password'
+        />
+        <Typography
+          fontWeight={600}
+          sx={{ cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        >
+          Sign In Instead?
+        </Typography>
+        <Button variant='contained' onClick={() => setIsLoading(!isLoading)}>
+          Register
+        </Button>
+      </Stack>
+      <IconButton
+        color='primary'
+        sx={{ position: 'absolute', top: -5, left: 10 }}
+        onClick={() => setSearchParams({ role: null })}
+      >
+        <ArrowBackIos />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default StudentForm;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,5 +1,121 @@
+import { Avatar, Button, Divider, Stack, Typography } from '@mui/material';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+import ParentForm from '../Components/auth/ParentForm';
+import StudentForm from '../Components/auth/StudentForm';
+import NFCLogo from '../Assets/Images/NFC Iet Logo.jfif';
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState(null);
+  const [xAxis, setXAxis] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const formAnimation = {
+    initial: {
+      x: xAxis,
+    },
+    animate: {
+      x: 0,
+      transition: {
+        bounce: 0,
+      },
+    },
+    exit: {
+      x: '-100vw',
+    },
+  };
+
+  const options = (
+    <Stack
+      direction='column'
+      alignItems='center'
+      component={motion.div}
+      key='Options'
+      variants={formAnimation}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      width='100%'
+    >
+      <Typography variant='h5' gutterBottom>
+        Register As
+      </Typography>
+      <Divider
+        sx={{
+          background: '#70231d',
+          height: '4px',
+          width: '25%',
+          borderRadius: '50px',
+          display: 'flex',
+          justifyContent: 'center',
+          marginBottom: '1em',
+        }}
+      />
+      <Stack
+        direction='column'
+        alignItems='center'
+        sx={{ paddingInline: '1em', width: '100%', gap: '1em' }}
+      >
+        <Button
+          variant='contained'
+          fullWidth
+          onClick={() => setSearchParams({ role: 'student' })}
+        >
+          Student
+        </Button>
+        <Button
+          variant='contained'
+          fullWidth
+          onClick={() => setSearchParams({ role: 'parent' })}
+        >
+          Parent
+        </Button>
+        <Typography
+          fontWeight={600}
+          sx={{ cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        >
+          Sign In Instead?
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+
+  useEffect(() => {
+    setXAxis('100vw');
+  }, []);
+
+  useEffect(() => {
+    const role = searchParams.get('role');
+    if (role === 'parent') setForm(<ParentForm animation={formAnimation} />);
+    else if (role === 'student')
+      setForm(<StudentForm animation={formAnimation} />);
+    else setForm(options);
+  }, [searchParams]);
+
+  return (
+    <Stack
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      width='100%'
+      minHeight='100vh'
+      overflow='hidden'
+    >
+      <Avatar
+        src={NFCLogo}
+        sx={{
+          width: 100,
+          height: 100,
+          objectFit: 'cover',
+        }}
+      />
+      <AnimatePresence mode='wait'>{form}</AnimatePresence>
+    </Stack>
+  );
 };
 
 export default Register;
