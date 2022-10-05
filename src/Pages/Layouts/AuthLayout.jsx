ca389c8e12d194220774612fe91ae5ad8f1ec682
Bilal Azhar <bilaazharcr7@gmail.com>
2022-10-05 13:19:45 +0500

    Some updates

--- src/Pages/Layouts/AuthLayout.jsx
+++ src/Pages/Layouts/AuthLayout.jsx
@@ -1,27 +1,27 @@
-import { Grid } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Grid } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
-import AuthLeftImage from '../../Assets/Images/auth-left-image.jpg';
+import AuthLeftImage from "../../Assets/Images/auth-left-image.jpg";
 
 const AuthLayout = ({ children }) => {
   return (
     <Grid
       container
-      width='100%'
-      minHeight='100vh'
-      maxHeight='100vh'
-      sx={{ overflow: 'hidden' }}
+      width="100%"
+      minHeight="100vh"
+      maxHeight="100vh"
+      // sx={{ overflow: "hidden" }}
     >
-      <Grid item md={7} lg={9} sx={{ display: { xs: 'none', md: 'block' } }}>
+      <Grid item md={7} lg={8} sx={{ display: { xs: "none", md: "block" } }}>
         <img
           src={AuthLeftImage}
-          alt='Auth Left'
-          width='100%'
-          height='100%'
-          style={{ objectFit: 'cover' }}
+          alt="Auth Left"
+          width="100%"
+          height="100%"
+          style={{ objectFit: "cover" }}
         />
       </Grid>
-      <Grid item xs={12} md={5} lg={3}>
+      <Grid item xs={12} md={5} lg={4}>
         <Outlet />
       </Grid>
     </Grid>
